a4c129dd90c2c262061264fc533ca3c99c827a2f
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2021-08-20 12:28:45 +0100

    Bug Fixes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# task2
+# HNG i8 Task 2
+The compulsory task is found in main.go
---
+++ main.go
@@ -0,0 +1,10 @@
+// This is the stated compulsory task
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("My name is Emmanuel Ezenwigbo!")
+}
